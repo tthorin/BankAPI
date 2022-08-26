095df6492f4a780d37f512a54c61f25667f73e3b
Thomas Thorin <thomas.thorin@gmail.com>
2022-08-26 14:59:29 +0200

    added logic to bankController

--- BankAPI/Controllers/BankController.cs
+++ BankAPI/Controllers/BankController.cs
@@ -1,41 +1,61 @@
 ﻿// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BankAPI.Controllers;
+
+using BankAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 [Route("[controller]")]
 [ApiController]
 public class BankController : ControllerBase
 {
+    private Account account;
+    public BankController(Account acc)
+    {
+        this.account = acc;
+    }
     // GET: api/<BankController>
     [HttpGet]
-    public IEnumerable<string> Get()
+    public string Get()
     {
-        return new string[] { "value1", "value2" };
+        var result = new BankResponse { success = true, newBalance = account.Balance };
+        var json = JsonConvert.SerializeObject(result);
+        return json;
     }
 
-    // GET api/<BankController>/5
-    [HttpGet("{id}")]
-    public string Get(int id)
-    {
-        return "value";
-    }
+    //// GET api/<BankController>/5
+    //[HttpGet("{id}")]
+    //public string Get(int id)
+    //{
+    //    return "value";
+    //}
 
-    // POST api/<BankController>
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-    }
+    //// POST api/<BankController>
+    //[HttpPost]
+    //public void Post([FromBody] string value)
+    //{
+    //}
 
     // PUT api/<BankController>/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
+    [HttpPut("withdraw/{amount}")]
+    public string Withdraw(decimal amount)
     {
+        var result = account.Withdraw(amount);
+        var json = JsonConvert.SerializeObject(result);
+        return json;
+    }
+    [HttpPut("insert/{amount}")]
+    public string Insert(decimal amount)
+    {
+        var result = account.Insert(amount);
+        var json = JsonConvert.SerializeObject(result);
+        return json;
     }
 
-    // DELETE api/<BankController>/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
+    //// DELETE api/<BankController>/5
+    //[HttpDelete("{id}")]
+    //public void Delete(int id)
+    //{
+    //}
 }
---
+++ BankAPI/Models/BankResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace BankAPI.Models;
+
+public class BankResponse
+{
+    public bool success;
+    public decimal newBalance;
+}
