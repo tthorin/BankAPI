a4e4d7d644b53910bee1e00e1710c2fe29a86f59
Thomas Thorin <thomas.thorin@gmail.com>
2022-08-26 13:40:10 +0200

    first code and tests

---
+++ BankAPI/Models/Account.cs
@@ -0,0 +1,27 @@
+﻿namespace BankAPI.Models;
+
+public class Account
+{
+    public decimal Balance { get; private set; } = 100;
+
+    public Account(int startBalance)
+    {
+        Balance = startBalance;
+    }
+
+    public bool Withdraw (decimal amount)
+    {
+        var result = false;
+        if (amount <= Balance && amount >= 0)
+        {
+            Balance -= amount;
+            result = true;
+        }
+        return result;
+    }
+
+    public bool Insert(decimal amount)
+    {
+        return true;
+    }
+}
---
+++ BankAPITests/Models/AccountTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using BankAPI.Models;
+
+namespace BankAPI.Models.Tests;
+
+public class AccountTests
+{
+    readonly Account sut = new(100);
+
+    [Fact()]
+    public void WithdrawLessThanBalanceShouldReturnTrue()
+    {
+        var expected = true;
+        var result = sut.Withdraw(50);
+
+        Assert.Equal(expected, result);
+    }
+    [Fact()]
+    public void WithdrawMoreThanBalanceShouldReturnFalse()
+    {
+        var expected = false;
+        var result = sut.Withdraw(150);
+
+        Assert.Equal(expected, result);
+    }
+    [Fact()]
+    public void WithdrawNegativeNumberShouldReturnFalse()
+    {
+        var expected = false;
+        var result = sut.Withdraw(-150M);
+
+        Assert.Equal(expected, result);
+    }
+    [Fact()]
+    public void InsertPositiveNumberShouldReturnTrue()
+    {
+        var expected = true;
+        var result = sut.Insert(150M);
+
+        Assert.Equal(expected, result);
+    }
+    [Fact()]
+    public void InsertNegativeNumberShouldReturnFalse()
+    {
+        var expected = false;
+        var result = sut.Insert(-150M);
+    }
+}
